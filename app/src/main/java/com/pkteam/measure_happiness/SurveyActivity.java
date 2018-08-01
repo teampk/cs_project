bb75b1554b48090d756de20db897d860b4c6c446
nuggy875 <jinugpaeng@gmail.com>
2018-08-01 22:33:27 +0900

    seek bar

--- app/src/main/java/com/pkteam/measure_happiness/SurveyActivity.java
+++ app/src/main/java/com/pkteam/measure_happiness/SurveyActivity.java
@@ -3,16 +3,67 @@ package com.pkteam.measure_happiness;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 /**
  * Created by paeng on 2018. 8. 1..
  */
 
 public class SurveyActivity extends AppCompatActivity {
+    private SeekBar sb1, sb2;
+    private TextView tv1, tv2;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey);
+
+        bindingView();
+
+
+    }
+
+    private void bindingView(){
+        sb1 = findViewById(R.id.sb_1);
+        sb2 = findViewById(R.id.sb_2);
+        tv1 = findViewById(R.id.tv_1);
+        tv2 = findViewById(R.id.tv_2);
+
+        sb1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                tv1.setText(String.valueOf(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        sb2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                tv2.setText(String.valueOf(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
 
 
     }
