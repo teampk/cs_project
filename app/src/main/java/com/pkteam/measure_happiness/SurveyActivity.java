e4eec30095dbe4e463fce810ba10400a07e12fac
nuggy875 <jinugpaeng@gmail.com>
2018-08-01 21:23:56 +0900

    git init

---
+++ app/src/main/java/com/pkteam/measure_happiness/SurveyActivity.java
@@ -0,0 +1,19 @@
+package com.pkteam.measure_happiness;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by paeng on 2018. 8. 1..
+ */
+
+public class SurveyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey);
+
+
+    }
+}
