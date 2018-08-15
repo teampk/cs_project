1cca128686de72566de1fe6ae20bfcf7d2173d4c
nuggy875 <jinugpaeng@gmail.com>
2018-08-15 21:51:37 +0900

    commit

---
+++ app/src/main/java/com/pkteam/measure_happiness/Splash.java
@@ -0,0 +1,19 @@
+package com.pkteam.measure_happiness;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by paeng on 15/08/2018.
+ */
+
+public class Splash extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+
+    }
+}
--- app/src/main/java/com/pkteam/measure_happiness/SurveyActivity.java
+++ app/src/main/java/com/pkteam/measure_happiness/SurveyActivity.java
@@ -1,6 +1,6 @@
 package com.pkteam.measure_happiness;
-
 import android.os.Bundle;
+
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
