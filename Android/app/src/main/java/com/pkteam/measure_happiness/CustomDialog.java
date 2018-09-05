c9a65bb0219cd5558293622c507c0e497c8afc49
nuggy875 <jinugpaeng@gmail.com>
2018-09-06 00:43:26 +0900

    dialog

--- Android/app/src/main/java/com/pkteam/measure_happiness/CustomDialog.java
+++ Android/app/src/main/java/com/pkteam/measure_happiness/CustomDialog.java
@@ -6,19 +6,22 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class CustomDialog {
     private Context context;
+    private TextView tvTitle, tvDescription, tvSatisfaction, tvQuestion1, tvQuestion2, tvQuestion3;
+    private SeekBar sbSatisfaction;
 
     public CustomDialog(Context context) {
         this.context = context;
     }
 
     // 호출할 다이얼로그 함수를 정의한다.
-    public void callFunction(final TextView main_label) {
+    public void callFunction(String dialogTitle, String dialogDescription, String dialogQuestion1, String dialogQuestion2, String dialogQuestion3) {
 
         // 커스텀 다이얼로그를 정의하기위해 Dialog클래스를 생성한다.
         final Dialog dlg = new Dialog(context);
@@ -33,18 +36,41 @@ public class CustomDialog {
         dlg.show();
 
         // 커스텀 다이얼로그의 각 위젯들을 정의한다.
-        final EditText message = (EditText) dlg.findViewById(R.id.mesgase);
-        final Button okButton = (Button) dlg.findViewById(R.id.okButton);
-        final Button cancelButton = (Button) dlg.findViewById(R.id.cancelButton);
+        final EditText message = dlg.findViewById(R.id.mesgase);
+        final Button okButton = dlg.findViewById(R.id.okButton);
+        final Button cancelButton = dlg.findViewById(R.id.cancelButton);
+
+        bindingView(dlg);
+
+        tvTitle.setText(dialogTitle);
+        tvDescription.setText(dialogDescription);
+        tvQuestion1.setText(dialogQuestion1);
+        tvQuestion2.setText(dialogQuestion2);
+        tvQuestion3.setText(dialogQuestion3);
+
+        sbSatisfaction.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tvSatisfaction.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
 
         okButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // '확인' 버튼 클릭시 메인 액티비티에서 설정한 main_label에
                 // 커스텀 다이얼로그에서 입력한 메시지를 대입한다.
-                main_label.setText(message.getText().toString());
-                Toast.makeText(context, "\"" +  message.getText().toString() + "\" 을 입력하였습니다.", Toast.LENGTH_SHORT).show();
-
                 // 커스텀 다이얼로그를 종료한다.
                 dlg.dismiss();
             }
@@ -60,6 +86,15 @@ public class CustomDialog {
         });
     }
 
+    private void bindingView(Dialog dlg){
+        tvTitle = dlg.findViewById(R.id.tv_title);
+        tvDescription = dlg.findViewById(R.id.tv_description);
+        tvSatisfaction = dlg.findViewById(R.id.tv_satisfaction);
+        tvQuestion1 = dlg.findViewById(R.id.tv_question_1);
+        tvQuestion2 = dlg.findViewById(R.id.tv_question_2);
+        tvQuestion3 = dlg.findViewById(R.id.tv_question_3);
+        sbSatisfaction = dlg.findViewById(R.id.sb_satisfaction);
+    }
 
 
 }
--- Android/app/src/main/java/com/pkteam/measure_happiness/View/SurveyActivity.java
+++ Android/app/src/main/java/com/pkteam/measure_happiness/View/SurveyActivity.java
@@ -28,43 +28,17 @@ public class SurveyActivity extends AppCompatActivity {
     private Button btnBack, btnSubmit;
     private ImageView ivQuestion1, ivQuestion2;
     private CustomDialog mCustomDialog;
-    private TextView tvTest;
+    private Button btnQuestion1, btnQuestion2;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey);
-
         bindingView();
 
 
     }
 
-
-    private void setSeekbar(final SeekBar sb, final TextView tv, final int ver) {
-        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                if (ver == 1){
-                    tv.setText(String.valueOf(i));
-
-                }else if (ver == 2) {
-                    tv.setText(String.valueOf(i*10));
-                }
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-    }
 
     private View.OnClickListener listener = new View.OnClickListener() {
         @Override
@@ -97,8 +71,12 @@ public class SurveyActivity extends AppCompatActivity {
                 case R.id.iv_question_1:
 
                     CustomDialog customDialog = new CustomDialog(SurveyActivity.this);
-                    customDialog.callFunction(tvTest);
-
+                    customDialog.callFunction(
+                            getString(R.string.happy_title_1),
+                            getString(R.string.happy_description_1),
+                            getString(R.string.happy_question_1_1),
+                            getString(R.string.happy_question_1_2),
+                            getString(R.string.happy_question_1_3));
 
                     break;
                 case R.id.iv_question_2:
@@ -111,6 +89,24 @@ public class SurveyActivity extends AppCompatActivity {
 
 
                     break;
+                case R.id.btn_question_1:
+                    CustomDialog customDialog1 = new CustomDialog(SurveyActivity.this);
+                    customDialog1.callFunction(
+                            getString(R.string.happy_title_1),
+                            getString(R.string.happy_description_1),
+                            getString(R.string.happy_question_1_1),
+                            getString(R.string.happy_question_1_2),
+                            getString(R.string.happy_question_1_3));
+                    break;
+                case R.id.btn_question_2:
+                    CustomDialog customDialog2 = new CustomDialog(SurveyActivity.this);
+                    customDialog2.callFunction(
+                            getString(R.string.happy_title_2),
+                            getString(R.string.happy_description_2),
+                            getString(R.string.happy_question_2_1),
+                            getString(R.string.happy_question_2_2),
+                            getString(R.string.happy_question_2_3));
+                    break;
                 default:
                     break;
             }
@@ -119,12 +115,16 @@ public class SurveyActivity extends AppCompatActivity {
 
 
     private void bindingView() {
-        tvTest = findViewById(R.id.tv_test);
 
         btnBack = findViewById(R.id.btn_back);
         btnBack.setOnClickListener(listener);
         btnSubmit = findViewById(R.id.btn_submit);
         btnSubmit.setOnClickListener(listener);
+
+        btnQuestion1 = findViewById(R.id.btn_question_1);
+        btnQuestion1.setOnClickListener(listener);
+        btnQuestion2 = findViewById(R.id.btn_question_2);
+        btnQuestion2.setOnClickListener(listener);
 
         ivQuestion1 = findViewById(R.id.iv_question_1);
         ivQuestion2 = findViewById(R.id.iv_question_2);
@@ -132,68 +132,6 @@ public class SurveyActivity extends AppCompatActivity {
         ivQuestion1.setOnClickListener(listener);
         ivQuestion2.setOnClickListener(listener);
 
-        SeekBar sb1_1 = findViewById(R.id.sb_1_1);
-        TextView tv1_1 = findViewById(R.id.tv_1_1);
-        SeekBar sb1_2 = findViewById(R.id.sb_1_2);
-        TextView tv1_2 = findViewById(R.id.tv_1_2);
-        SeekBar sb2_1 = findViewById(R.id.sb_2_1);
-        TextView tv2_1 = findViewById(R.id.tv_2_1);
-        SeekBar sb2_2 = findViewById(R.id.sb_2_2);
-        TextView tv2_2 = findViewById(R.id.tv_2_2);
-        SeekBar sb3_1 = findViewById(R.id.sb_3_1);
-        TextView tv3_1 = findViewById(R.id.tv_3_1);
-        SeekBar sb3_2 = findViewById(R.id.sb_3_2);
-        TextView tv3_2 = findViewById(R.id.tv_3_2);
-        SeekBar sb4_1 = findViewById(R.id.sb_4_1);
-        TextView tv4_1 = findViewById(R.id.tv_4_1);
-        SeekBar sb4_2 = findViewById(R.id.sb_4_2);
-        TextView tv4_2 = findViewById(R.id.tv_4_2);
-        SeekBar sb5_1 = findViewById(R.id.sb_5_1);
-        TextView tv5_1 = findViewById(R.id.tv_5_1);
-        SeekBar sb5_2 = findViewById(R.id.sb_5_2);
-        TextView tv5_2 = findViewById(R.id.tv_5_2);
-        SeekBar sb6_1 = findViewById(R.id.sb_6_1);
-        TextView tv6_1 = findViewById(R.id.tv_6_1);
-        SeekBar sb6_2 = findViewById(R.id.sb_6_2);
-        TextView tv6_2 = findViewById(R.id.tv_6_2);
-        SeekBar sb7_1 = findViewById(R.id.sb_7_1);
-        TextView tv7_1 = findViewById(R.id.tv_7_1);
-        SeekBar sb7_2 = findViewById(R.id.sb_7_2);
-        TextView tv7_2 = findViewById(R.id.tv_7_2);
-        SeekBar sb8_1 = findViewById(R.id.sb_8_1);
-        TextView tv8_1 = findViewById(R.id.tv_8_1);
-        SeekBar sb8_2 = findViewById(R.id.sb_8_2);
-        TextView tv8_2 = findViewById(R.id.tv_8_2);
-        SeekBar sb9_1 = findViewById(R.id.sb_9_1);
-        TextView tv9_1 = findViewById(R.id.tv_9_1);
-        SeekBar sb9_2 = findViewById(R.id.sb_9_2);
-        TextView tv9_2 = findViewById(R.id.tv_9_2);
-        SeekBar sb10_1 = findViewById(R.id.sb_10_1);
-        TextView tv10_1 = findViewById(R.id.tv_10_1);
-        SeekBar sb10_2 = findViewById(R.id.sb_10_2);
-        TextView tv10_2 = findViewById(R.id.tv_10_2);
-
-
-        setSeekbar(sb1_1, tv1_1, 1);
-        setSeekbar(sb1_2, tv1_2, 2);
-        setSeekbar(sb2_1, tv2_1, 1);
-        setSeekbar(sb2_2, tv2_2, 2);
-        setSeekbar(sb3_1, tv3_1, 1);
-        setSeekbar(sb3_2, tv3_2, 2);
-        setSeekbar(sb4_1, tv4_1, 1);
-        setSeekbar(sb4_2, tv4_2, 2);
-        setSeekbar(sb5_1, tv5_1, 1);
-        setSeekbar(sb5_2, tv5_2, 2);
-        setSeekbar(sb6_1, tv6_1, 1);
-        setSeekbar(sb6_2, tv6_2, 2);
-        setSeekbar(sb7_1, tv7_1, 1);
-        setSeekbar(sb7_2, tv7_2, 2);
-        setSeekbar(sb8_1, tv8_1, 1);
-        setSeekbar(sb8_2, tv8_2, 2);
-        setSeekbar(sb9_1, tv9_1, 1);
-        setSeekbar(sb9_2, tv9_2, 2);
-        setSeekbar(sb10_1, tv10_1, 1);
-        setSeekbar(sb10_2, tv10_2, 2);
 
     }
 }
