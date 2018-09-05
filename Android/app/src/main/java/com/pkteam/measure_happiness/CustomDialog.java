7a2bab056fad5a3f7f90901aa6943dcf5395fcbf
nuggy875 <jinugpaeng@gmail.com>
2018-09-06 04:50:20 +0900

    survey complete

--- Android/app/src/main/java/com/pkteam/measure_happiness/CustomDialog.java
+++ Android/app/src/main/java/com/pkteam/measure_happiness/CustomDialog.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,13 +16,14 @@ public class CustomDialog {
     private Context context;
     private TextView tvTitle, tvDescription, tvSatisfaction, tvQuestion1, tvQuestion2, tvQuestion3;
     private SeekBar sbSatisfaction;
+    private RadioButton rb1a, rb1b, rb1c, rb1d, rb1e, rb2a, rb2b, rb2c, rb2d, rb2e, rb3a, rb3b, rb3c, rb3d, rb3e;
 
     public CustomDialog(Context context) {
         this.context = context;
     }
 
     // 호출할 다이얼로그 함수를 정의한다.
-    public void callFunction(String dialogTitle, String dialogDescription, String dialogQuestion1, String dialogQuestion2, String dialogQuestion3) {
+    public void callFunction(final TextView tvScore, String dialogTitle, String dialogDescription, String dialogQuestion1, String dialogQuestion2, String dialogQuestion3) {
 
         // 커스텀 다이얼로그를 정의하기위해 Dialog클래스를 생성한다.
         final Dialog dlg = new Dialog(context);
@@ -69,10 +71,19 @@ public class CustomDialog {
         okButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // '확인' 버튼 클릭시 메인 액티비티에서 설정한 main_label에
-                // 커스텀 다이얼로그에서 입력한 메시지를 대입한다.
-                // 커스텀 다이얼로그를 종료한다.
-                dlg.dismiss();
+                if (checkSubmit()) {
+                    int score = 1;
+                    score += calculateScoreRadioButton(rb1a, rb1b, rb1c, rb1d, rb1e);
+                    score += calculateScoreRadioButton(rb2a, rb2b, rb2c, rb2d, rb2e);
+                    score += calculateScoreRadioButton(rb3a, rb3b, rb3c, rb3d, rb3e);
+                    Toast.makeText(context, "중요도=" + tvSatisfaction.getText() + " 만족도=" + score, Toast.LENGTH_SHORT).show();
+
+                    int finalScore = getFinalScore(score);
+
+                    tvScore.setText(String.valueOf(finalScore));
+
+                    dlg.dismiss();
+                }
             }
         });
         cancelButton.setOnClickListener(new View.OnClickListener() {
@@ -85,6 +96,43 @@ public class CustomDialog {
             }
         });
     }
+    private int calculateScoreRadioButton(RadioButton rbA, RadioButton rbB, RadioButton rbC, RadioButton rbD, RadioButton rbE){
+        int score=0;
+
+        if (rbA.isChecked()){
+            score = 0;
+        }else if (rbB.isChecked()){
+            score = 8;
+        }else if (rbC.isChecked()){
+            score = 16;
+        }else if (rbD.isChecked()){
+            score = 25;
+        }else if (rbE.isChecked()){
+            score = 33;
+        }
+
+        return score;
+    }
+    private int getFinalScore(int score){
+        int finalScore=0;
+        if (score < 14){
+            finalScore = 1;
+        }else if (score < 28){
+            finalScore = 2;
+        }else if (score < 42){
+            finalScore = 3;
+        }else if (score < 56){
+            finalScore = 4;
+        }else if (score < 70){
+            finalScore = 5;
+        }else if (score < 84){
+            finalScore = 6;
+        }else if (score <= 100){
+            finalScore = 7;
+        }
+        return finalScore;
+    }
+
 
     private void bindingView(Dialog dlg){
         tvTitle = dlg.findViewById(R.id.tv_title);
@@ -94,6 +142,42 @@ public class CustomDialog {
         tvQuestion2 = dlg.findViewById(R.id.tv_question_2);
         tvQuestion3 = dlg.findViewById(R.id.tv_question_3);
         sbSatisfaction = dlg.findViewById(R.id.sb_satisfaction);
+
+        rb1a = dlg.findViewById(R.id.rb_1_a);
+        rb1b = dlg.findViewById(R.id.rb_1_b);
+        rb1c = dlg.findViewById(R.id.rb_1_c);
+        rb1d = dlg.findViewById(R.id.rb_1_d);
+        rb1e = dlg.findViewById(R.id.rb_1_e);
+
+        rb2a = dlg.findViewById(R.id.rb_2_a);
+        rb2b = dlg.findViewById(R.id.rb_2_b);
+        rb2c = dlg.findViewById(R.id.rb_2_c);
+        rb2d = dlg.findViewById(R.id.rb_2_d);
+        rb2e = dlg.findViewById(R.id.rb_2_e);
+
+        rb3a = dlg.findViewById(R.id.rb_3_a);
+        rb3b = dlg.findViewById(R.id.rb_3_b);
+        rb3c = dlg.findViewById(R.id.rb_3_c);
+        rb3d = dlg.findViewById(R.id.rb_3_d);
+        rb3e = dlg.findViewById(R.id.rb_3_e);
+
+    }
+
+    private boolean checkSubmit(){
+
+        if (!rb1a.isChecked() && !rb1b.isChecked() && !rb1c.isChecked() && !rb1d.isChecked() && !rb1e.isChecked()) {
+            Toast.makeText(context, "1번 문항을 체크해주세요.", Toast.LENGTH_SHORT).show();
+            return false;
+        }else if (!rb2a.isChecked() && !rb2b.isChecked() && !rb2c.isChecked() && !rb2d.isChecked() && !rb2e.isChecked()){
+            Toast.makeText(context, "2번 문항을 체크해주세요.", Toast.LENGTH_SHORT).show();
+            return false;
+        }else if (!rb3a.isChecked() && !rb3b.isChecked() && !rb3c.isChecked() && !rb3d.isChecked() && !rb3e.isChecked()){
+            Toast.makeText(context, "3번 문항을 체크해주세요.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+
+        return true;
     }
 
 
--- Android/app/src/main/java/com/pkteam/measure_happiness/View/SurveyActivity.java
+++ Android/app/src/main/java/com/pkteam/measure_happiness/View/SurveyActivity.java
@@ -8,6 +8,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.pkteam.measure_happiness.CustomDialog;
@@ -20,8 +21,8 @@ import com.pkteam.measure_happiness.R;
 public class SurveyActivity extends AppCompatActivity {
     private Button btnBack, btnSubmit;
     private CustomDialog mCustomDialog;
-    private Button btnQuestion1, btnQuestion2, btnQuestion3, btnQuestion4, btnQuestion5,
-            btnQuestion6, btnQuestion7, btnQuestion8, btnQuestion9, btnQuestion10;
+    private TextView tvScore1, tvScore2, tvScore3, tvScore4, tvScore5,
+            tvScore6, tvScore7, tvScore8, tvScore9, tvScore10;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -64,6 +65,7 @@ public class SurveyActivity extends AppCompatActivity {
                 case R.id.btn_question_1:
                     CustomDialog customDialog1 = new CustomDialog(SurveyActivity.this);
                     customDialog1.callFunction(
+                            tvScore1,
                             getString(R.string.happy_title_1),
                             getString(R.string.happy_description_1),
                             getString(R.string.happy_question_1_1),
@@ -73,6 +75,7 @@ public class SurveyActivity extends AppCompatActivity {
                 case R.id.btn_question_2:
                     CustomDialog customDialog2 = new CustomDialog(SurveyActivity.this);
                     customDialog2.callFunction(
+                            tvScore2,
                             getString(R.string.happy_title_2),
                             getString(R.string.happy_description_2),
                             getString(R.string.happy_question_2_1),
@@ -82,11 +85,82 @@ public class SurveyActivity extends AppCompatActivity {
                 case R.id.btn_question_3:
                     CustomDialog customDialog3 = new CustomDialog(SurveyActivity.this);
                     customDialog3.callFunction(
+                            tvScore3,
                             getString(R.string.happy_title_3),
                             getString(R.string.happy_description_3),
                             getString(R.string.happy_question_3_1),
                             getString(R.string.happy_question_3_2),
                             getString(R.string.happy_question_3_3));
+                    break;
+                case R.id.btn_question_4:
+                    CustomDialog customDialog4 = new CustomDialog(SurveyActivity.this);
+                    customDialog4.callFunction(
+                            tvScore4,
+                            getString(R.string.happy_title_4),
+                            getString(R.string.happy_description_4),
+                            getString(R.string.happy_question_4_1),
+                            getString(R.string.happy_question_4_2),
+                            getString(R.string.happy_question_4_3));
+                    break;
+                case R.id.btn_question_5:
+                    CustomDialog customDialog5 = new CustomDialog(SurveyActivity.this);
+                    customDialog5.callFunction(
+                            tvScore5,
+                            getString(R.string.happy_title_5),
+                            getString(R.string.happy_description_5),
+                            getString(R.string.happy_question_5_1),
+                            getString(R.string.happy_question_5_2),
+                            getString(R.string.happy_question_5_3));
+                    break;
+                case R.id.btn_question_6:
+                    CustomDialog customDialog6 = new CustomDialog(SurveyActivity.this);
+                    customDialog6.callFunction(
+                            tvScore6,
+                            getString(R.string.happy_title_6),
+                            getString(R.string.happy_description_6),
+                            getString(R.string.happy_question_6_1),
+                            getString(R.string.happy_question_6_2),
+                            getString(R.string.happy_question_6_3));
+                    break;
+                case R.id.btn_question_7:
+                    CustomDialog customDialog7 = new CustomDialog(SurveyActivity.this);
+                    customDialog7.callFunction(
+                            tvScore7,
+                            getString(R.string.happy_title_7),
+                            getString(R.string.happy_description_7),
+                            getString(R.string.happy_question_7_1),
+                            getString(R.string.happy_question_7_2),
+                            getString(R.string.happy_question_7_3));
+                    break;
+                case R.id.btn_question_8:
+                    CustomDialog customDialog8 = new CustomDialog(SurveyActivity.this);
+                    customDialog8.callFunction(
+                            tvScore8,
+                            getString(R.string.happy_title_8),
+                            getString(R.string.happy_description_8),
+                            getString(R.string.happy_question_8_1),
+                            getString(R.string.happy_question_8_2),
+                            getString(R.string.happy_question_8_3));
+                    break;
+                case R.id.btn_question_9:
+                    CustomDialog customDialog9 = new CustomDialog(SurveyActivity.this);
+                    customDialog9.callFunction(
+                            tvScore9,
+                            getString(R.string.happy_title_9),
+                            getString(R.string.happy_description_9),
+                            getString(R.string.happy_question_9_1),
+                            getString(R.string.happy_question_9_2),
+                            getString(R.string.happy_question_9_3));
+                    break;
+                case R.id.btn_question_10:
+                    CustomDialog customDialog10 = new CustomDialog(SurveyActivity.this);
+                    customDialog10.callFunction(
+                            tvScore10,
+                            getString(R.string.happy_title_10),
+                            getString(R.string.happy_description_10),
+                            getString(R.string.happy_question_10_1),
+                            getString(R.string.happy_question_10_2),
+                            getString(R.string.happy_question_10_3));
                     break;
                 default:
                     break;
@@ -97,17 +171,43 @@ public class SurveyActivity extends AppCompatActivity {
 
     private void bindingView() {
 
-        btnBack = findViewById(R.id.btn_back);
+        Button btnBack = findViewById(R.id.btn_back);
         btnBack.setOnClickListener(listener);
-        btnSubmit = findViewById(R.id.btn_submit);
+        Button btnSubmit = findViewById(R.id.btn_submit);
         btnSubmit.setOnClickListener(listener);
 
-        btnQuestion1 = findViewById(R.id.btn_question_1);
+        Button btnQuestion1 = findViewById(R.id.btn_question_1);
         btnQuestion1.setOnClickListener(listener);
-        btnQuestion2 = findViewById(R.id.btn_question_2);
+        Button btnQuestion2 = findViewById(R.id.btn_question_2);
         btnQuestion2.setOnClickListener(listener);
-        btnQuestion3 = findViewById(R.id.btn_question_3);
+        Button btnQuestion3 = findViewById(R.id.btn_question_3);
         btnQuestion3.setOnClickListener(listener);
+        Button btnQuestion4 = findViewById(R.id.btn_question_4);
+        btnQuestion4.setOnClickListener(listener);
+        Button btnQuestion5 = findViewById(R.id.btn_question_5);
+        btnQuestion5.setOnClickListener(listener);
+        Button btnQuestion6 = findViewById(R.id.btn_question_6);
+        btnQuestion6.setOnClickListener(listener);
+        Button btnQuestion7 = findViewById(R.id.btn_question_7);
+        btnQuestion7.setOnClickListener(listener);
+        Button btnQuestion8 = findViewById(R.id.btn_question_8);
+        btnQuestion8.setOnClickListener(listener);
+        Button btnQuestion9 = findViewById(R.id.btn_question_9);
+        btnQuestion9.setOnClickListener(listener);
+        Button btnQuestion10 = findViewById(R.id.btn_question_10);
+        btnQuestion10.setOnClickListener(listener);
+
+
+        tvScore1 = findViewById(R.id.tv_score_1);
+        tvScore2 = findViewById(R.id.tv_score_2);
+        tvScore3 = findViewById(R.id.tv_score_3);
+        tvScore4 = findViewById(R.id.tv_score_4);
+        tvScore5 = findViewById(R.id.tv_score_5);
+        tvScore6 = findViewById(R.id.tv_score_6);
+        tvScore7 = findViewById(R.id.tv_score_7);
+        tvScore8 = findViewById(R.id.tv_score_8);
+        tvScore9 = findViewById(R.id.tv_score_9);
+        tvScore10 = findViewById(R.id.tv_score_10);
 
 
 
