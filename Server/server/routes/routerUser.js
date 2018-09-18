bbe722ef86721666dfe1200c520184e3dc7f0d99
nuggy875 <jinugpaeng@gmail.com>
2018-09-19 00:08:47 +0900

    sign in

--- Server/server/routes/routerUser.js
+++ Server/server/routes/routerUser.js
@@ -1,11 +1,45 @@
 var express = require('express');
 var router = express.Router();
+
+var auth = require('basic-auth');
+var jwt = require('jsonwebtoken');
+var fs = require('fs');
+
 var registerUser = require('../functions/registerUser');
 var profile = require('../functions/profile');
+
+var config = require('../config/config.json');
+var multer = require('multer');
+var path = require('path');
 
 router.get('/', function(req,res){
   res.send('this is user page');
 
+});
+
+router.post('/authenticate', function(req, res) {
+  var credentials = auth(req);
+  if (!credentials) {
+    res.status(400).json({
+      message: 'Invalid Request !'
+    });
+  } else {
+    login.loginUser(credentials.name, credentials.pass)
+      .then(function(result) {
+        var token = jwt.sign(result, config.secret, {
+          expiresIn: 1440
+        });
+        res.status(result.status).json({
+          message: result.message,
+          token: token
+        });
+      })
+      .catch(function(err) {
+        res.status(err.status).json({
+          message: err.message
+        });
+      });
+  }
 });
 
 router.post('/users', function(req,res){
